59b102b2de97ac7347f4fdc7170ffd9a8fa06bf9
Neo Qi <qixiaochen@gmail.com>
2011-05-22 06:44:49 +0000

    稍微修改了下LoginServlet，求反馈！！
    TODO：需要在index.jsp上对输入进行规范

--- StudentPortal/src/com/tongji/j2ee/sp/LoginServlet.java
+++ StudentPortal/src/com/tongji/j2ee/sp/LoginServlet.java
@@ -10,34 +10,68 @@ import javax.servlet.http.HttpSession;
 
 import model.Studentinfo;
 import model.StudentinfoDAO;
+import model.Teacherinfo;
+import model.TeacherinfoDAO;
 
 public class LoginServlet extends HttpServlet {
 	
-	private StudentinfoDAO siset = new StudentinfoDAO();
-	private Studentinfo current;
+	//when to initialize the DAO is a problem
+	private StudentinfoDAO studentinfoDAO = new StudentinfoDAO();
+	private Studentinfo studentinfo;
+	private TeacherinfoDAO teacherinfoDAO = new TeacherinfoDAO();
+	private Teacherinfo teacherinfo;
 
 	public void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-		HttpSession hs = request.getSession();
-		current = siset.findById(request.getParameter("userID"));
-		if(current == null){
-			request.setAttribute("errorInformation", "The user does not exist!");
-			request.getRequestDispatcher("index.jsp").forward(request, response);
-		}
-		hs.setAttribute("user", current);
 		
-		String passwd = current.getPassword();
-		if(passwd.equals(request.getParameter("password"))){
-			System.out.println(current.getEmail());
-			if(current.getEmail().equals("")){
-				
-				request.getRequestDispatcher("register.jsp").forward(request, response);
+		String id = request.getParameter("userID");
+		String password = request.getParameter("password");
+		
+		//for student_id
+		if(id.length()==6){
+			studentinfo = studentinfoDAO.findById(id);
+			if(studentinfo == null){
+				request.setAttribute("errorInformation", "The user does not exist!");
+				request.getRequestDispatcher("index.jsp").forward(request, response);
 				return;
+			}else{
+				if(password.equals(studentinfo.getPassword())){
+					if(studentinfo.getEmail().equals("")){
+						request.getRequestDispatcher("register.jsp").forward(request, response);
+						return;
+					}
+					//TODO: replace by student.jsp
+					request.getRequestDispatcher("teacher.jsp").forward(request, response);
+				}else{
+					request.setAttribute("errorInformation", "The password is not right!");
+					request.getRequestDispatcher("index.jsp").forward(request, response);
+				}
 			}
-			System.out.println(current.getName());
-			request.getRequestDispatcher("teacher.jsp").forward(request, response);
-		}else{
-			request.getRequestDispatcher("index.jsp").forward(request, response);
 		}
+		
+		//for teacher_id
+		if(id.length()==10){
+			teacherinfo = teacherinfoDAO.findById(id);
+			if(teacherinfo == null){
+				request.setAttribute("errorInformation", "The user does not exist!");
+				request.getRequestDispatcher("index.jsp").forward(request, response);
+				return;
+			}else{
+				if(password.equals(teacherinfo.getPassword())){
+					if(teacherinfo.getEmail().equals("")){
+						request.getRequestDispatcher("register.jsp").forward(request, response);
+						return;
+					}
+					request.getRequestDispatcher("teacher.jsp").forward(request, response);
+				}else{
+					request.setAttribute("errorInformation", "The password is not right!");
+					request.getRequestDispatcher("index.jsp").forward(request, response);
+				}
+			}
+		}
+		
+		//TODO:for admin_id
 	}
 }
+
+
